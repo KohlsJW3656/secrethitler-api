59757c18b399b537c3e85afcb7568fcf6eec01ca
kohlsjw3656 <kohlsjw3656@gmail.com>
2021-09-04 00:05:35 -0500

    Removed file

--- make.sql
+++
@@ -1,7 +0,0 @@
-DROP DATABASE IF EXISTS secrethitler;
-DROP USER IF EXISTS shUser@localhost;
-
-CREATE DATABASE secrethitler CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER 'shUser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'wQynVfAg1$';
-GRANT ALL ON secrethitler.* TO 'shUser'@'localhost';
-FLUSH PRIVILEGES;
