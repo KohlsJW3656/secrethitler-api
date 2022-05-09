88e001861c05863fd02914a227a2f594f6e53cd9
Jonas Kohls <kohlsjw3656@uwec.edu>
2022-05-09 17:25:40 -0500

    Moved voting and officals tls to use the game_user_action table

--- schema.sql
+++ schema.sql
@@ -34,7 +34,7 @@ CREATE TABLE policy (
 CREATE TABLE action (
   action_id SERIAL PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
-  game_action BOOLEAN NOT NULL
+  system_action BOOLEAN NOT NULL
 );
 
 CREATE TABLE rule (
@@ -47,6 +47,7 @@ CREATE TABLE game (
   game_id SERIAL PRIMARY KEY,
   name VARCHAR(40) NOT NULL,
   private_game BOOLEAN NOT NULL,
+  turn INT NOT NULL,
   created_time DATETIME NOT NULL,
   start_time DATETIME DEFAULT NULL,
   end_time DATETIME DEFAULT NULL,
@@ -68,12 +69,6 @@ CREATE TABLE game_user (
   role_id INT NOT NULL DEFAULT 0,
   username VARCHAR(200) NOT NULL,
   ready BOOLEAN NOT NULL DEFAULT 0,
-  president BOOLEAN NOT NULL DEFAULT 0,
-  chancellor BOOLEAN NOT NULL DEFAULT 0,
-  prev_president BOOLEAN NOT NULL DEFAULT 0,
-  prev_chancellor BOOLEAN NOT NULL DEFAULT 0,
-  confirmed_not_hitler BOOLEAN NOT NULL DEFAULT 0,
-  ballot BOOLEAN DEFAULT NULL,
   FOREIGN KEY(game_id) REFERENCES game(game_id),
   FOREIGN KEY(user_id) REFERENCES user(user_id),
   FOREIGN KEY(role_id) REFERENCES role(role_id)
@@ -94,6 +89,7 @@ CREATE TABLE game_user_action (
   game_user_action_id SERIAL PRIMARY KEY,
   game_user_id BIGINT UNSIGNED NOT NULL,
   action_id BIGINT UNSIGNED NOT NULL,
+  turn INT NOT NULL,
   game_policy_id BIGINT UNSIGNED,
   target_user_id BIGINT UNSIGNED,
   action_time DATETIME NOT NULL,
@@ -133,52 +129,52 @@ INSERT INTO policy(fascist) VALUES (1);
 INSERT INTO policy(fascist) VALUES (1);
 INSERT INTO policy(fascist) VALUES (1);
 
-INSERT INTO action(name, game_action) VALUES ("Game began", 1);
-INSERT INTO action(name, game_action) VALUES ("Fascists won", 1);
-INSERT INTO action(name, game_action) VALUES ("Liberals won", 1);
-INSERT INTO action(name, game_action) VALUES ("Game ended", 1);
+INSERT INTO action(name, system_action) VALUES ("Game began", 1);
+INSERT INTO action(name, system_action) VALUES ("Fascists won", 1);
+INSERT INTO action(name, system_action) VALUES ("Liberals won", 1);
+INSERT INTO action(name, system_action) VALUES ("Game ended", 1);
 
 /* Election */
-INSERT INTO action(name, game_action) VALUES ("Election began", 1);
-INSERT INTO action(name, game_action) VALUES ("Vote passed", 1);
-INSERT INTO action(name, game_action) VALUES ("Vote failed", 1);
-INSERT INTO action(name, game_action) VALUES ("Election tracker increased", 1);
-INSERT INTO action(name, game_action) VALUES ("Election tracker reset", 1);
-INSERT INTO action(name, game_action) VALUES ("Top policy enacted", 1);
-INSERT INTO action(name, game_action) VALUES ("Election ended", 1);
+INSERT INTO action(name, system_action) VALUES ("Election began", 1);
+INSERT INTO action(name, system_action) VALUES ("Vote passed", 1);
+INSERT INTO action(name, system_action) VALUES ("Vote failed", 1);
+INSERT INTO action(name, system_action) VALUES ("Election tracker increased", 1);
+INSERT INTO action(name, system_action) VALUES ("Election tracker reset", 1);
+INSERT INTO action(name, system_action) VALUES ("Top policy enacted", 1);
+INSERT INTO action(name, system_action) VALUES ("Election ended", 1);
 
-INSERT INTO action(name, game_action) VALUES ("Nominated for president", 0);
-INSERT INTO action(name, game_action) VALUES ("Nominated for chancellor", 0);
-INSERT INTO action(name, game_action) VALUES ("Voted Ja!", 0);
-INSERT INTO action(name, game_action) VALUES ("Voted Nein!", 0);
-INSERT INTO action(name, game_action) VALUES ("Elected president", 0);
-INSERT INTO action(name, game_action) VALUES ("Elected chancellor", 0);
+INSERT INTO action(name, system_action) VALUES ("Nominated for president", 0);
+INSERT INTO action(name, system_action) VALUES ("Nominated for chancellor", 0);
+INSERT INTO action(name, system_action) VALUES ("Voted Ja!", 0);
+INSERT INTO action(name, system_action) VALUES ("Voted Nein!", 0);
+INSERT INTO action(name, system_action) VALUES ("Elected president", 0);
+INSERT INTO action(name, system_action) VALUES ("Elected chancellor", 0);
 
 /* Legislative Session */
-INSERT INTO action(name, game_action) VALUES ("Legislative Session began", 1);
-INSERT INTO action(name, game_action) VALUES ("Fascist policy enacted", 1);
-INSERT INTO action(name, game_action) VALUES ("Liberal policy enacted", 1);
-INSERT INTO action(name, game_action) VALUES ("Legislative Session ended", 1);
+INSERT INTO action(name, system_action) VALUES ("Legislative Session began", 1);
+INSERT INTO action(name, system_action) VALUES ("Fascist policy enacted", 1);
+INSERT INTO action(name, system_action) VALUES ("Liberal policy enacted", 1);
+INSERT INTO action(name, system_action) VALUES ("Legislative Session ended", 1);
 
-INSERT INTO action(name, game_action) VALUES ("Discarded", 0);
-INSERT INTO action(name, game_action) VALUES ("Enacted", 0);
-INSERT INTO action(name, game_action) VALUES ("Requested veto", 0);
-INSERT INTO action(name, game_action) VALUES ("Denied veto", 0);
-INSERT INTO action(name, game_action) VALUES ("Accepted veto", 0);
+INSERT INTO action(name, system_action) VALUES ("Discarded", 0);
+INSERT INTO action(name, system_action) VALUES ("Enacted", 0);
+INSERT INTO action(name, system_action) VALUES ("Requested veto", 0);
+INSERT INTO action(name, system_action) VALUES ("Denied veto", 0);
+INSERT INTO action(name, system_action) VALUES ("Accepted veto", 0);
 
 /* Executive Action */
-INSERT INTO action(name, game_action) VALUES ("Executive action began", 1);
-INSERT INTO action(name, game_action) VALUES ("Investigate Loyalty", 1);
-INSERT INTO action(name, game_action) VALUES ("Call Special Election", 1);
-INSERT INTO action(name, game_action) VALUES ("Policy Peek", 1);
-INSERT INTO action(name, game_action) VALUES ("Execution", 1);
-INSERT INTO action(name, game_action) VALUES ("Veto Power", 1);
-INSERT INTO action(name, game_action) VALUES ("Executive action ended", 1);
+INSERT INTO action(name, system_action) VALUES ("Executive action began", 1);
+INSERT INTO action(name, system_action) VALUES ("Investigate Loyalty", 1);
+INSERT INTO action(name, system_action) VALUES ("Call Special Election", 1);
+INSERT INTO action(name, system_action) VALUES ("Policy Peek", 1);
+INSERT INTO action(name, system_action) VALUES ("Execution", 1);
+INSERT INTO action(name, system_action) VALUES ("Veto Power", 1);
+INSERT INTO action(name, system_action) VALUES ("Executive action ended", 1);
 
-INSERT INTO action(name, game_action) VALUES ("Investigated loyalty", 0);
-INSERT INTO action(name, game_action) VALUES ("Called Special Election", 0);
-INSERT INTO action(name, game_action) VALUES ("Examined top 3 policies", 0);
-INSERT INTO action(name, game_action) VALUES ("Executed", 0);
+INSERT INTO action(name, system_action) VALUES ("Investigated loyalty", 0);
+INSERT INTO action(name, system_action) VALUES ("Called Special Election", 0);
+INSERT INTO action(name, system_action) VALUES ("Examined top 3 policies", 0);
+INSERT INTO action(name, system_action) VALUES ("Executed", 0);
 
 INSERT INTO rule(name, description) VALUES ("Shadow Democracy", "Player ballots are not shown during and after voting");
 INSERT INTO rule(name, description) VALUES ("Political Leverage", "If the group rejects three governments in a row, any power granted by the enacted policy is granted to the previously elected president");
